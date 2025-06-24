fd71c058b9b01e8b2a2a2d03b168e22dc19c19ae
KhalilAmamri <KhalilAmamri@outlook.com>
2025-06-24 14:01:00 +0100

    add date input and page request for YallaKora match center

--- yallakora_scraper.py
+++ yallakora_scraper.py
@@ -2,3 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
+date = input("please enter the date in YYYY-MM-DD format: ")
+page = requests.get(f"https://www.yallakora.com/Match-Center?date={date}")
+# this will be the URL for the match center page of YallaKora for the given date
